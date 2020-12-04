3c766e4757c6fdb93eb631ace3ae4414f05d3e9c
EdisonLeung <edisonleung2003@outlook.com>
2020-12-03 20:38:59 -0800

    renamed variables

--- RobotSimUnity/Assets/tank_drive_controller.cs
+++ RobotSimUnity/Assets/tank_drive_controller.cs
@@ -9,6 +9,10 @@ public class tank_drive_controller : MonoBehaviour
 
     private string[] dataText;
 
+    private const float ROBOT_SPEED_MULTIPLIER = 0.125f;
+    private const float ROBOT_TANK_ROTATION_MULTIPLIER = 3f;
+    private const float ROBOT_SWERVE_ROTATION_MULTIPLIER = 5f;
+
     void Start()
     {
         dataText = System.IO.File.ReadAllText(@"C:\Users\ediso\vs-workspace\SimulatorTest\data.txt").Split(' ');
@@ -35,44 +39,21 @@ public class tank_drive_controller : MonoBehaviour
 
             float currentAngle = (float)Math.PI * transform.rotation.eulerAngles.y/180;  
 
-            transform.position += new Vector3((float)Math.Sin((double)currentAngle) * forward * 0.125f, 0, 
-                                            (float)Math.Cos((double)currentAngle) * forward * 0.125f);
-            transform.Rotate(0, rotation * 3, 0, Space.Self);
+            transform.position += new Vector3((float)Math.Sin((double)currentAngle) * forward * ROBOT_SPEED_MULTIPLIER, 0, 
+                                            (float)Math.Cos((double)currentAngle) * forward * ROBOT_SPEED_MULTIPLIER);
+            transform.Rotate(0, rotation * ROBOT_TANK_ROTATION_MULTIPLIER, 0, Space.Self);
         }else if(swerve_mode){
             float forward = motorValues[0];
             float strafe = motorValues[1];
             float rotation = motorValues[2];
 
-            transform.position += new Vector3(strafe * 0.125f, 0, forward * 0.125f);
-            transform.Rotate(0, rotation * 5, 0, Space.Self);
+            transform.position += new Vector3(strafe * ROBOT_SPEED_MULTIPLIER, 0, forward * ROBOT_SPEED_MULTIPLIER);
+            transform.Rotate(0, rotation * ROBOT_SWERVE_ROTATION_MULTIPLIER, 0, Space.Self);
         }
 
         foreach(float num in motorValues){
             Debug.Log(num);
         }
-
-        // transform.position = new Vector3(robotPosition[1], 0.8f, robotPosition[0]);
-        // transform.eulerAngles = new Vector3(0 , robotPosition[2], 0);
-        // float inputY = deadBand(Input.GetAxis("Vertical"));
-        // float inputX = deadBand(Input.GetAxis("Horizontal"));
-        // float inputStrafe = deadBand(Input.GetAxis("Strafe"));
-
-        // Debug.Log("Rotation: " + inputX);
-        // Debug.Log("Forward: " +inputY);
-
-        // float currentAngle = (float)Math.PI * transform.rotation.eulerAngles.y/180;  
-
-        // if(swerve_mode){
-        //     transform.position += new Vector3(inputStrafe * Time.deltaTime * SPEED, 0, 
-        //                                       inputY * Time.deltaTime * SPEED);
-        // }else{
-
-        // }
-        // // transform.position += new Vector3((float)Math.Sin((double)currentAngle) * inputY * Time.deltaTime * SPEED, 0, 
-        // //                                   (float)Math.Cos((double)currentAngle) * inputY * Time.deltaTime * SPEED);
-        // transform.Rotate(0, inputX * Time.deltaTime * ROTATE_SPEED, 0, Space.World);
-
-
     }
 
     string calculateDriveType(string driveType){
@@ -80,11 +61,5 @@ public class tank_drive_controller : MonoBehaviour
             return "Swerve";
         }
         return "Tank";
-    }
-    float deadBand(float input){
-        if(Math.Abs(input) > 0.15){
-            return input;
-        }
-        return 0;
     }
 }
